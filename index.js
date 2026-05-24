b952902b8a8d31048e33d337d35461ae5c3770f3
OtsoH <otsoheinamaki@gmail.com>
2026-05-25 01:30:22 +0300

    complete exercise 16

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { errorHandler } = require('./util/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const authorsRouter = require('./controllers/authors')
 
 const app = express()
 app.use(express.json())
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/authors', authorsRouter)
 
 app.use(errorHandler)
 
