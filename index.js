2a5a98f7805a0aea0239622368a9ce4ca1b57f96
OtsoH <otsoheinamaki@gmail.com>
2026-05-25 01:52:55 +0300

    complete exercise 17

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const authorsRouter = require('./controllers/authors')
+const resetRouter = require('./controllers/reset')
 
 const app = express()
 app.use(express.json())
@@ -13,6 +14,9 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/authors', authorsRouter)
+app.use('/api/reset', resetRouter)
+
+app.get('/', (req, res) => res.status(200).end())
 
 app.use(errorHandler)
 
