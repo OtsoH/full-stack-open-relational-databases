f9ff9475dcb901b390a0c22f4941aab0af7050e2
OtsoH <otsoheinamaki@gmail.com>
2026-05-26 00:18:44 +0300

    complete exercise 19

--- models/blog.js
+++ models/blog.js
@@ -6,6 +6,13 @@ const Blog = sequelize.define('blog', {
   url: { type: DataTypes.TEXT, allowNull: false },
   title: { type: DataTypes.TEXT, allowNull: false },
   likes: { type: DataTypes.INTEGER, defaultValue: 0 },
+  year: {
+    type: DataTypes.INTEGER,
+    validate: {
+      min: { args: [1991], msg: 'Year must be at least 1991' },
+      max: { args: [new Date().getFullYear()], msg: `Year cannot be greater than ${new Date().getFullYear()}` }
+    }
+  },
   userId: {
     type: DataTypes.INTEGER,
     references: { model: 'users', key: 'id' }
